ff88036ee96cf0093fb143c959cbc2daaa545f0e
Ana Florentino <abmflorentino@gmail.com>
2024-05-16 11:35:22 -0300

    config uglify

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,13 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify'); 
 
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'));
+}
 
 // functions
 function styles() {
@@ -18,9 +24,10 @@ function images() {
 }
 
 // exports:
-exports.default = gulp.parallel(styles, images);
+exports.default = gulp.parallel(styles, images, scripts);
 
 exports.watch = function() {
     gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+    gulp.watch('./src/scripts/*.js', gulp.parallel(scripts))
 }
 
